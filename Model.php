f601794b1eefd9b5b6e0f6cba9cf72a923d9af07
anil3a <anilprz3@gmail.com>
2019-07-25 08:06:08 +1000

    get and set fitness data and validate access token for google fitness

--- Model.php
+++ Model.php
@@ -95,4 +95,130 @@ Class Model extends Config {
         return $this;
     }
 
+    public function getUrlAuth()
+    {
+        return $this->getClient()->createAuthUrl();
+    }
+
+    public function initUserFitnessData()
+    {
+        $db = new stdClass();
+        $fitnessData = $db->query(
+            "SELECT `field_fitness_id`,`field_user_stored_access_token_code`
+            FROM `table_users`
+            WHERE `field_user_id` = ". $this->getUserId() .
+            "AND `field_user_active` = 1
+            ORDER BY `field_fitness_id` desc"
+        );
+        if( !empty( $fitnessData ) && is_array( $fitnessData ) )
+        {
+            $this->setFitnessData( $fitnessData );
+        }
+        else {
+            $this->setFitnessData( [] );
+        }
+        return $this;
+    }
+
+    public function updateUserAccessToken( Integer $field_fitness_id, String $field_user_stored_access_token_code )
+    {
+        $db = new \stdClass();
+        return $db->query(
+            "UPDATE `table_users`
+            SET `field_user_stored_access_token_code` = \"". $field_user_stored_access_token_code ."\""
+            ."WHERE `field_fitness_id` = ". $field_fitness_id
+        );
+    }
+
+    public function initClientAssessed()
+    {
+        $client = $this->getClient();
+        $fitnessData = $this->getFitnessData();
+
+        if( empty( $fitnessData ) )
+        {
+            $this->initUserFitnessData();
+            $fitnessData = $this->getFitnessData();
+        }
+        if ( empty( $fitnessData['access_token'] ) ) return $client;
+
+        $accessToken = json_decode( $fitnessData['access_token'], true );
+
+        if( !is_array( $accessToken ) || empty( $accessToken['access_token'] ) )
+        {
+            return $client;
+        }
+        $client->setAccessToken( $accessToken );
+
+        if ( $client->isAccessTokenExpired() )
+        {
+            $v = $client->getRefreshToken();
+            if ( !empty( $v ) )
+            {
+                try {
+                    $client->fetchAccessTokenWithRefreshToken( $v );
+                    $this->updateUserAccessToken( $fitnessData['id_fitness'], json_encode( $client->getAccessToken() ) );
+                } catch ( \Exception $e ) {
+                    throw new \Exception( $e->getMessage() );
+                }
+            }
+        }
+        $this->setClient( $client );
+        return $this;
+    }
+
+    public function isAuthenticated()
+    {
+        $client = $this->getClient();
+        $fitnessData = $this->getFitnessData();
+
+        if( empty( $fitnessData ) )
+        {
+            $this->initUserFitnessData();
+            $fitnessData = $this->getFitnessData();
+        }
+
+        if ( empty( $fitnessData['access_token'] ) ) return $client;
+
+        $accessToken = json_decode( $fitnessData['access_token'], true );
+
+        if( !is_array( $accessToken ) || empty( $accessToken['access_token'] ) )
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public function setGoogleUserAuthentication( String $code )
+    {
+        $client = $this->getClient();
+        $fitnessData = $this->getFitnessData();
+
+        if( empty( $fitnessData ) )
+        {
+            $this->initUserFitnessData();
+            $fitnessData = $this->getFitnessData();
+        }
+
+        $accessToken = $client->fetchAccessTokenWithAuthCode( $code );
+        if( !empty( $accessToken ) )
+        {
+            $accessToken = json_encode( $accessToken );
+        } else {
+            $accessToken = '';
+        }
+        $this->updateUserAccessToken( $this->getUserId(), $accessToken );
+        return $this;
+    }
+
+    public function getFitnessAcess()
+    {
+        if( $this->isAuthenticated() )
+        {
+            $this->initClientAssessed();
+        } else {
+            $this->getClient();
+        }
+        return $this;
+    }
 }
