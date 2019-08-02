c11f9329887b33b695d175eab488f88eced72723
anil3a <anilprz3@gmail.com>
2019-08-02 12:22:37 +1000

    added safe json encoder as Helper

--- Helper.php
+++ Helper.php
@@ -12,4 +12,55 @@ Class Helper
         }
         return $r;
     }
+
+    /**
+     * Safe Json Encoder
+     *
+     * @param Array|Object $value
+     * @param integer $options
+     * @param integer $depth
+     * @param boolean $utfErrorFlag
+     * @return void
+     */
+    public function safe_json_encode( $value, INT $options = 0, INT $depth = 512, BOOL $utfErrorFlag = false )
+    {
+        $encoded = json_encode( $value, $options, $depth );
+        switch ( json_last_error() )
+        {
+            case JSON_ERROR_NONE:
+                return $encoded;
+            case JSON_ERROR_DEPTH:
+                throw new \Exception( 'Maximum stack depth exceeded' );
+            case JSON_ERROR_STATE_MISMATCH:
+                throw new \Exception( 'Underflow or the modes mismatch' );
+            case JSON_ERROR_CTRL_CHAR:
+                throw new \Exception( 'Unexpected control character found' );
+            case JSON_ERROR_SYNTAX:
+                throw new \Exception( 'Syntax error, malformed JSON' );
+            case JSON_ERROR_UTF8:
+                $clean = utf8ize( $value );
+                if( $utfErrorFlag )
+                {
+                    throw new \Exception( 'UTF8 encoding error' );
+                }
+                return safe_json_encode( $clean, $options, $depth, true );
+            default:
+                throw new \Exception( 'Unknown error' );
+        }
+    }
+
+    public function utf8ize( $mixed )
+    {
+        if( is_array( $mixed ) )
+        {
+            foreach( $mixed as $key => $value )
+            {
+                $mixed[$key] = utf8ize( $value );
+            }
+        } else if( is_string ( $mixed) )
+        {
+            return utf8_encode( $mixed );
+        }
+        return $mixed;
+    }
 }
